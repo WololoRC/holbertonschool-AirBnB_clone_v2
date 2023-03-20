551b6a3e2e5e4bd69972c14d0085faf2c6b3c6ca
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-03-20 20:05:07 -0300

    fix errors

--- models/city.py
+++ models/city.py
@@ -7,9 +7,9 @@ from os import getenv
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
+    __tablename__ = 'cities'
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        __tablename__ = 'cities'
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('state.id'))
 
--- models/state.py
+++ models/state.py
@@ -14,7 +14,8 @@ class State(BaseModel, Base):
     # for DBStorage
     if getenv("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(128), nullable=False)
-        cities = relationship('City', backref="state", cascade="all, delete, delete-orphan")
+        cities = relationship('City', backref="state",
+                              cascade="all, delete, delete-orphan")
     else:
         name = ""
 

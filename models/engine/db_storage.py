33f91b91aded2bc2896567dec9af12342734442f
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-03-20 20:45:56 -0300

    add all function

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,16 +3,15 @@
 """
 
 from sqlalchemy import (create_engine)
-from os import getenv
 import os
+from os import getenv
 from models.base_model import Base
 import models
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 class DBstorage:
-
-#Private class attributes:
+    #Private class attributes:
     __engine = None
     __session = None
 
@@ -23,30 +22,34 @@ class DBstorage:
         passwd = os.getenv('HBNB_MYSQL_PWD')
         host = os.getenv('HBNB_MYSQL_HOST')
         database = os.getenv('HBNB_MYSQL_DB')
-        environment = os.getenv('HBNB_ENV')
-        
-        """user = os.environ.get['HBNB_MYSQL_USER']
-        passwd = os.environ.get['HBNB_MYSQL_PWD']
-        host = os.environ.get['HBNB_MYSQL_HOST']
-        database = os.environ.get['HBNB_MYSQL_DB']
-        environment = os.environ.get['HBNB_ENV']"""
 
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}:{}'
                                       .format(user, passwd, host, database),
                                       pool_pre_ping=True)
 
-        if environment == 'test':
+        if os.getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
         """some"""
-        # query on the current database session
-        self.__session.query(models.classes[cls]).all()
-        
-        # if cls=None, query all types of objects (User, State, City, Amenity, Place and Review)
-            # key = <class-name>.<object-id>
-            # value = object
-        
+        if type(cls) is str:
+            cls = eval(cls) # returns the result of the evaluated expression
+
+        dictionary = {}
+
+        if cls is None:
+            for table in self.__all_classes:
+                object = self.__session.query(table)
+                for one_obj in object:
+                    class_name = one_obj.__class__.__name__
+                    key = class_name + '.' + one_obj.id
+                    dictionary[key] = one_obj
+        else:
+            rows = self.__session.query(cls).all()
+            for obj in rows:
+                key = obj.__class__.__name__ + '.' + obj.id
+                dictionary[k] = obj
+        return dictionary
         # this method must return a dictionary: (like FileStorage)
 
     def new(self, obj):

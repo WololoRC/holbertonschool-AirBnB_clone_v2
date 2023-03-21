2298b21fd39ed10887a0f36a41d575381af4af63
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-03-21 00:01:25 -0300

    fix imports

--- models/__init__.py
+++ models/__init__.py
@@ -4,10 +4,10 @@
 from os import getenv
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
-    from models.engine import db_storage
-    storage = db_storage.DBStorage()
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
 else:
-    from models.engine import file_storage
-    storage = file_storage.FileStorage()
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 
 storage.reload()

7b71b0b2886c5ab5c11a8afb3efb973630133d5d
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-03-20 23:03:10 -0300

    add changes

--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import os
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
@@ -12,20 +13,24 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
 
     # for DBStorage
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', backref="state",
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref="state",
                               cascade="all, delete, delete-orphan")
+
+    """
     else:
         name = ""
+    """
 
+    if os.getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
+            """some"""
             list_of_cities = []
             # relationship between State and City
-            for city in models.storage.all("City").values():
+            for key, value in models.storage.all().values():
                 # with state_id equals to the current State.id
-                if city.state_id == self.id:
-                    list_of_cities.append(city)
+                if value.__class__.__name__ == 'City' and value.state_id == self.id:
+                    list_of_cities.append(value)
             return list_of_cities
             # list of City with state_id equals to the current State.id

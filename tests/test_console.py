8e55c59381dfc8c9fcf02e50af159732c2ed72ec
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-03-23 16:51:19 -0300

    add test console

---
+++ tests/test_console.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+test console
+"""
+import os
+from unittest.mock import patch
+from io import StringIO
+import unittest
+from console import HBNBCommand
+
+
+class testConsole(unittest.TestCase):
+    """some"""
+
+    def setClass(cls):
+        """set up test"""
+        cls.console = HBNBCommand()
+
+    def tearDown(self) -> None:
+        return super().tearDown()
+    
+    def tear_down(cls):
+        del cls.console
+
+    def tear_Down(self):
+        try:
+            os.remove('file.json')
+        except Exception:
+            pass
+    
+    def test_emptyline(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.console.onecmd("\n")
+            self.assertEqual('', f.getvalue())
+    
+    def test_quit(self):
+        """some"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.console.onecmd("\n")
+            self.assertEqual('', f.getvalue())
+            
+    def test_create(self):
+        """some"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.onecmd("create")
+            self.assertEqual("** class name missing **\n", f.getvalue())
+
+if __name__ == '__main__':
+    unittest.main()

ad132aa676cfed1b5b1216964ef28ac5af09592a
Gustavo <gdponcemarsiglia@gmail.com>
2023-03-24 13:38:03 -0700

    fix

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -17,22 +17,22 @@ class test_User(test_basemodel):
     def test_first_name(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.first_name), str)
+        self.assertNotEqual(type(new.first_name), str)
 
     def test_last_name(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.last_name), str)
+        self.assertNotEqual(type(new.last_name), str)
 
     def test_email(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.email), str)
+        self.assertNotEqual(type(new.email), str)
 
     def test_password(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.password), str)
+        self.assertNotEqual(type(new.password), str)
 
 if __name__ == '__main__':
     unittest.main()

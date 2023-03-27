ce9b592ac4e4ac0305b66a18eb335e7a67728ab6
WololoRC <wololoRC@proton.me>
2023-03-27 16:07:12 -0300

    a

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """my first flask application"""
 from flask import Flask
-import subprocess
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
-    strict_slashes=False
+    """Say hello"""
+    strict_slashes = False
     return "Hello HBNB!"
+
 
 app.run(host='0.0.0.0', port=5000)

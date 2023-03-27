4d319732ba4c3bcfdf6ccf0c00dd54f282de5912
WololoRC <wololoRC@proton.me>
2023-03-27 18:49:30 -0300

    tsk2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -5,7 +5,6 @@ Routes:
 /: display “Hello HBNB!”
 /hbnb: display “HBNB”
 /c/<text>: display “C ” followed by the value of the text variable
-(replace underscore _ symbols with a space )
 """
 
 from flask import Flask

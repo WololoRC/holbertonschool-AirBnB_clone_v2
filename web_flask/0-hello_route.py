72b33984850b0c2086875462a74c37683d9c255c
WololoRC <wololoRC@proton.me>
2023-03-27 15:33:41 -0300

    add init and task0 files

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from flask import Flask
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    strict_slashes=False
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
